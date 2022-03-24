bc7df8bce278f78d1cedeef27f71c4ac1f458b4d
Artur Kudaev <oldoldskoolgamer@gmail.com>
2022-03-24 22:12:02 +0300

    verticalMessage 2.0

--- vertMess.js
+++ vertMess.js
@@ -1,8 +1,11 @@
 function showVerticalMessage(message) {
     
-    if (message.includes('u', 0))
-        message = message[0].toUpperCase() + message.slice(1, 10);
-        
+    if (message.includes('м', 0) )
+        message = message[0].toUpperCase() + message.slice(1);
+    
+    if (message.length > 10)    
+        message = message.slice(0, 10)
+
     for (let i = 0; i < message.length; i++) {
         
         console.log(message[i] + '\n');
